504f557bb5a2ef82e8351bfba023670675fed690
keithgabriel <khojhogabby@gmail.com>
2024-10-15 17:11:38 +0000

    0x00. ES6 Basics

---
+++ 0x00-ES6_basic/0-main.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import { taskFirst, taskNext } from './0-constants.js';
+
+// eslint-disable-next-line jest/require-hook
+console.log(`${taskFirst()} ${taskNext()}`);
---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,8 @@
+/* eslint-disable jest/require-hook */
+// eslint-disable-next-line import/extensions
+import getSumOfHoods from './3-default-parameter.js';
+
+// eslint-disable-next-line jest/require-hook
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
